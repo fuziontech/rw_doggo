6646689358cb08cf02e97bca63495b0663ae8870
James Greenhill <fuziontech@gmail.com>
2022-09-05 19:00:30 -0700

    enable job control

--- .fly/start.sh
+++ .fly/start.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-set -exm
+set -ex
 
 if [ -n $MIGRATE_ON_BOOT ]; then
   $(dirname $0)/migrate.sh
 fi
 
 # inspired from https://fly.io/docs/app-guides/multiple-processes/
+set -m # to make job control work
 npx rw-server --port ${PORT} $@ &
 yarn rw exec updateQueue &
 fg %1 # gross!

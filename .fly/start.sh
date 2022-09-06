12a8d3e9ccfb57d378063cc1f3beaa991af36853
James Greenhill <fuziontech@gmail.com>
2022-09-05 18:55:13 -0700

    bash script massaging

--- .fly/start.sh
+++ .fly/start.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-set -ex
+set -exm
 
 if [ -n $MIGRATE_ON_BOOT ]; then
   $(dirname $0)/migrate.sh
 fi
 
 # inspired from https://fly.io/docs/app-guides/multiple-processes/
-set -m # to make job control work
 npx rw-server --port ${PORT} $@ &
 yarn rw exec updateQueue &
 fg %1 # gross!

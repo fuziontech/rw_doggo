4370e81668b78870771efc2094565496cd38d4f0
James Greenhill <fuziontech@gmail.com>
2022-09-05 19:03:11 -0700

    remove job control

--- .fly/start.sh
+++ .fly/start.sh
@@ -7,7 +7,5 @@ if [ -n $MIGRATE_ON_BOOT ]; then
 fi
 
 # inspired from https://fly.io/docs/app-guides/multiple-processes/
-set -m # to make job control work
-npx rw-server --port ${PORT} $@ &
 yarn rw exec updateQueue &
-fg %1 # gross!
+npx rw-server --port ${PORT} $@
